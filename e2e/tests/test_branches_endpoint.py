3104ce31929cbd918ec7b870f2be08f90f4a71c1
Eliasz Slusarczyk <eliasz@ergodicity.io>
2018-07-05 22:14:56 +0100

    e2e: add premature branches test

---
+++ e2e/tests/test_branches_endpoint.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import os
+import grpc
+
+import erepro.api.branches.v1.branches_pb2 as branches
+import erepro.api.branches.v1.branches_pb2_grpc as branches_grpc
+
+
+def run():
+    with grpc.insecure_channel(os.getenv('HOSTPORT', 'localhost:9090')) as ch:
+        stub = branches_grpc.BranchesStub(ch)
+
+        res = stub.ListBranches(branches.ListBranchesRequest(page_size=10))
+
+        assert res is not None
+
+
+if __name__ == '__main__':
+    run()
