297f16fa6f6fd39e1779785cc7eec0234b3308f9
Eliasz Slusarczyk <eliasz@ergodicity.io>
2018-07-08 23:35:21 +0100

    add simple branches endpoint grpc test

--- e2e/tests/test_branches_endpoint.py
+++ e2e/tests/test_branches_endpoint.py
@@ -10,10 +10,9 @@ import erepro.api.branches.v1.branches_pb2_grpc as branches_grpc
 def run():
     with grpc.insecure_channel(os.getenv('HOSTPORT', 'localhost:9090')) as ch:
         stub = branches_grpc.BranchesStub(ch)
-
         res = stub.ListBranches(branches.ListBranchesRequest(page_size=10))
-
         assert res is not None
+        print(res)
 
 
 if __name__ == '__main__':
