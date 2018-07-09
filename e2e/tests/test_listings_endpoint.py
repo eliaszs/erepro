c145b41468adb5bd6b55b84bdabba904ee27351a
Eliasz Slusarczyk <eliasz@ergodicity.io>
2018-07-09 08:52:54 +0100

    add create listing to the listings pytest

--- e2e/tests/test_listings_endpoint.py
+++ e2e/tests/test_listings_endpoint.py
@@ -10,5 +10,22 @@ import erepro.api.listings.v1.listings_pb2_grpc as listings_grpc
 def test_list_listings():
     with grpc.insecure_channel(os.getenv('HOSTPORT', 'localhost:9090')) as ch:
         stub = listings_grpc.ListingsStub(ch)
+
+        res = listings.CreateListingRequest(listing=listings.Listing(
+            name='3 bed house',
+            address='radison road, Isle-of-wight',
+            no_beds=3,
+            no_baths=1,
+            price=600000,
+            status='sale'
+        ))
+        print(res)
+
+        print('--------')
+
         res = stub.ListListings(listings.ListListingsRequest(page_size=10))
         print(res)
+
+
+if __name__ == '__main__':
+    test_list_listings()
